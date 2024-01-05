6c30d3a672e3544b8030eb763643f413ba3e6444
tobitprince <prince.tobit@strathmore.edu>
2024-01-05 16:47:51 +0300

    0x00-ES6_basic

---
+++ 0x00-ES6_basic/first.js
@@ -0,0 +1,14 @@
+export function take(){
+    const something='I am in misery';
+    return take;
+
+}
+export function another(){
+    let world='hoping I do not die of depression';
+    world += hope();
+    return world
+}
+export function hope(){
+    return 'waiting on God';
+
+}
